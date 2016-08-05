6ded6534da52e8c81d80dbeff89a13949723324b
kuangzhiqiang [邝志强] <kuangzhiqiang@xiaomi.com>
2016-08-05 20:19:25 +0800

    close popover

--- public/js/common.js
+++ public/js/common.js
@@ -81,4 +81,25 @@ $(function () {
 
     // initDataTable
     initDataTable();
+
+    // close popover
+    $('body').on('click', function (e) {
+        var target = $(e.target);
+        $('[data-toggle="popover"]').each(function () {
+            var $this = $(this);
+            if (!$this.is(target) && $this.has(target).length === 0 && $('.popover').has(target).length === 0) {
+                var popoverElement = $this.data('bs.popover').tip();
+                var popoverWasVisible = popoverElement.is(':visible');
+
+                if (popoverWasVisible) {
+                    $this.popover('hide');
+                    // double clicking required to reshow the popover if it was open, so perform one click now
+                    var inState = $this.data('bs.popover').inState;
+                    for(var i in inState){
+                        inState[i] = false;
+                    }
+                }
+            }
+        });
+    });
 });
